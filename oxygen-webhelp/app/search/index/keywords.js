81fe23ebd37fe35594fd8787afc2bf155cd51b08
Camil0086 <kamil.nowak.0086@gmail.com>
2021-03-24 22:38:46 +0100

    new_topics_update

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,10 +1,15 @@
-define(function() {var keywords=[{w:"Our",p:["p0"]},{w:"friend",p:["p0"]},{w:"the",p:["p0","p4"]},{w:"tree",p:["p0"]},{w:"Solid",p:["p1","p3"]},{w:"wood",p:["p1","p3"]},{w:"On",p:["p2"]},{w:"trips",p:["p2"]},{w:"to",p:["p2","p4"]},{w:"India",p:["p2"]},{w:"furniture",p:["p3","p4"]},{w:"stands",p:["p3"]},{w:"for",p:["p3"]},{w:"environmental",p:["p3"]},{w:"friendliness",p:["p3"]},{w:"and",p:["p3"]},{w:"sustainability",p:["p3"]},{w:"From",p:["p4"]},{w:"website",p:["p4"]},{w:"living",p:["p4"]},{w:"room,",p:["p4"]},{w:"how",p:["p4"]},{w:"does",p:["p4"]},{w:"my",p:["p4"]},{w:"favorite",p:["p4"]},{w:"piece",p:["p4"]},{w:"of",p:["p4"]},{w:"get",p:["p4"]},{w:"home?",p:["p4"]}];
+define(function() {var keywords=[{w:"Our",p:["p0"]},{w:"friend",p:["p0"]},{w:"the",p:["p0","p4"]},{w:"tree",p:["p0"]},{w:"Solid",p:["p1","p3","p9"]},{w:"wood",p:["p1","p3","p6"]},{w:"On",p:["p2"]},{w:"trips",p:["p2"]},{w:"to",p:["p2","p4"]},{w:"India",p:["p2"]},{w:"furniture",p:["p3","p4","p9"]},{w:"stands",p:["p3"]},{w:"for",p:["p3","p9"]},{w:"environmental",p:["p3"]},{w:"friendliness",p:["p3"]},{w:"and",p:["p3","p9"]},{w:"sustainability",p:["p3"]},{w:"From",p:["p4"]},{w:"website",p:["p4"]},{w:"living",p:["p4"]},{w:"room,",p:["p4"]},{w:"how",p:["p4","p5"]},{w:"does",p:["p4","p5"]},{w:"my",p:["p4"]},{w:"favorite",p:["p4"]},{w:"piece",p:["p4"]},{w:"of",p:["p4"]},{w:"get",p:["p4"]},{w:"home?",p:["p4"]},{w:"Complaints",p:["p5"]},{w:"-",p:["p5"]},{w:"it",p:["p5"]},{w:"work?",p:["p5"]},{w:"Removing",p:["p6","p7","p8"]},{w:"worms",p:["p6"]},{w:"scratches",p:["p7"]},{w:"stains",p:["p8"]},{w:"First",p:["p9"]},{w:"aid",p:["p9"]},{w:"tips",p:["p9"]},{w:"tricks",p:["p9"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3];
 ph["p1"]=[4, 5];
 ph["p2"]=[6, 7, 8, 9];
 ph["p3"]=[4, 5, 10, 11, 12, 13, 14, 15, 16];
 ph["p4"]=[17, 2, 18, 8, 2, 19, 20, 21, 22, 23, 24, 25, 26, 10, 27, 8, 23, 28];
+ph["p5"]=[29, 30, 21, 22, 31, 32];
+ph["p6"]=[33, 5, 34];
+ph["p7"]=[33, 35];
+ph["p8"]=[33, 36];
+ph["p9"]=[37, 38, 39, 15, 40, 12, 4, 10];
      return {
          keywords: keywords,
          ph: ph

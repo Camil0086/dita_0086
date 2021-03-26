9a93380e94dd08d052cdcc21b8868bec04a2372a
Camil0086 <kamil.nowak.0086@gmail.com>
2021-03-26 22:42:48 +0100

    fix_update

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {12:4,0:-1,1:-1,11:-1,3:-1,4:-1,9:11,7:-1,10:11,5:-1,6:-1,2:-1,13:4,8:11};
+return {0:-1,4:-1,1:-1,3:-1,5:-1,9:11,7:-1,6:-1,13:4,12:4,2:-1,8:11,10:11,11:-1};
 });
